18ee05bf4b9b8c209c2c8af59fd735464a2d13b9
eotla <108768789+eotla@users.noreply.github.com>
2022-07-05 16:47:48 -0700

    First Commit
    Controlled Rocket movement and one added obstacle

---
+++ ProjectBoost/Assets/Scripts/Movement.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] float mainThrust = 1200f;
+    [SerializeField] float rotationThrust = 250f;
+    Rigidbody myRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+
+    void ProcessThrust()
+    {
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            myRigidbody.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        }
+
+    }
+
+    void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D))
+        {
+            //Do Nothing
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            ApplyRotation(rotationThrust);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(-rotationThrust);
+        }
+
+    }
+
+    private void ApplyRotation(float rotationThisFrame)
+    {
+        myRigidbody.freezeRotation = true; // freezing rotation so that we can override physics.
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+        myRigidbody.freezeRotation = false; // unfreezing rotation so that we can let physics take over.
+    }
+
+}
